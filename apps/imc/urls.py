510f5b57d36977502369c844f777d4b1a8ee7c50
eudivan44 <eudivan44@gmail.com>
2024-10-28 13:50:28 +0000

    created home page, started imc page

--- apps/imc/urls.py
+++ apps/imc/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import home_page, imc_page
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path('', home_page, name='home'),
+    path('imc/', imc_page, name='imc'),
 ]
